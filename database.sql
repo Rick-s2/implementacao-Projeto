d8200f255d94b43064148e9e875c2887ce15a9af
Guillermo <guillermotoro61@gmail.com>
2023-04-27 08:24:01 -0300

    SQL de usuarios, enderecos e contatos criados

--- database.sql
+++ database.sql
@@ -1,10 +1,35 @@
-CREATE TABLE usuarios ( 
+CREATE TABLE tb_enderecos (
+  id_endereco int AUTO_INCREMENT,
+  cep CHAR(9) NOT NULL,
+  logradouro VARCHAR(255) NOT NULL,
+  numero_endereco int NOT NULL,
+  bairro VARCHAR(100) NOT NULL,
+  cidade VARCHAR(100) NOT NULL,
+  pais VARCHAR(45) NOT NULL,
+  PRIMARY KEY (id_endereco)
+);
+
+CREATE TABLE tb_contatos (
+  id_contato int AUTO_INCREMENT,
+  telefone CHAR(10) NOT NULL,
+  celular CHAR(11) NOT NULL,
+  email VARCHAR(100),
+  PRIMARY KEY (id_contato)
+);
+
+CREATE TABLE tb_usuarios ( 
   id_usuario int AUTO_INCREMENT, 
-  nome_usuario varchar(70) NOT NULL, 
+  id_endereco int,
+  id_contato int,
+  nome varchar(70) NOT NULL, 
+  cpf char(11) NOT NULL,
   login varchar(15) NOT NULL,
   senha varchar(15) NOT NULL,
   papeis varchar(255),
-  PRIMARY KEY (id_usuario) 
+  status_usuario varchar(70) NOT NULL,
+  PRIMARY KEY (id_usuario),
+  CONSTRAINT fk_id_endereco FOREIGN KEY (id_endereco) REFERENCES tb_endereco (id_endereco),
+  CONSTRAINT fk_id_contato FOREIGN KEY (id_contato) REFERENCES tb_contato (id_contato)
 );
 ALTER TABLE usuarios ADD CONSTRAINT uk_usuarios UNIQUE KEY (login);
 
