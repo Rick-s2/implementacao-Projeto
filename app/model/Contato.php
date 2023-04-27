f9a80b9b419f2ee24581abe12f184a448de3e9bb
Guillermo <guillermotoro61@gmail.com>
2023-04-27 07:56:16 -0300

    Classes Endereco e Contato criadas

---
+++ app/model/Contato.php
@@ -0,0 +1,70 @@
+<?php
+
+class Contato{
+
+    private $telefone;
+    private $celular;
+    private $email;
+
+    
+
+    /**
+     * Get the value of telefone
+     */ 
+    public function getTelefone()
+    {
+        return $this->telefone;
+    }
+
+    /**
+     * Set the value of telefone
+     *
+     * @return  self
+     */ 
+    public function setTelefone($telefone)
+    {
+        $this->telefone = $telefone;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of celular
+     */ 
+    public function getCelular()
+    {
+        return $this->celular;
+    }
+
+    /**
+     * Set the value of celular
+     *
+     * @return  self
+     */ 
+    public function setCelular($celular)
+    {
+        $this->celular = $celular;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */ 
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */ 
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
---
+++ app/model/Endereco.php
@@ -0,0 +1,133 @@
+<?php
+
+class Endereco{
+
+    private $cep;
+    private $logradouro;
+    private $numeroEndereco;
+    private $bairro;
+    private $cidade;
+    private $pais;
+
+    
+
+    /**
+     * Get the value of cep
+     */ 
+    public function getCep()
+    {
+        return $this->cep;
+    }
+
+    /**
+     * Set the value of cep
+     *
+     * @return  self
+     */ 
+    public function setCep($cep)
+    {
+        $this->cep = $cep;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of logradouro
+     */ 
+    public function getLogradouro()
+    {
+        return $this->logradouro;
+    }
+
+    /**
+     * Set the value of logradouro
+     *
+     * @return  self
+     */ 
+    public function setLogradouro($logradouro)
+    {
+        $this->logradouro = $logradouro;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of numeroEndereco
+     */ 
+    public function getNumeroEndereco()
+    {
+        return $this->numeroEndereco;
+    }
+
+    /**
+     * Set the value of numeroEndereco
+     *
+     * @return  self
+     */ 
+    public function setNumeroEndereco($numeroEndereco)
+    {
+        $this->numeroEndereco = $numeroEndereco;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of bairro
+     */ 
+    public function getBairro()
+    {
+        return $this->bairro;
+    }
+
+    /**
+     * Set the value of bairro
+     *
+     * @return  self
+     */ 
+    public function setBairro($bairro)
+    {
+        $this->bairro = $bairro;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of cidade
+     */ 
+    public function getCidade()
+    {
+        return $this->cidade;
+    }
+
+    /**
+     * Set the value of cidade
+     *
+     * @return  self
+     */ 
+    public function setCidade($cidade)
+    {
+        $this->cidade = $cidade;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of pais
+     */ 
+    public function getPais()
+    {
+        return $this->pais;
+    }
+
+    /**
+     * Set the value of pais
+     *
+     * @return  self
+     */ 
+    public function setPais($pais)
+    {
+        $this->pais = $pais;
+
+        return $this;
+    }
+}
