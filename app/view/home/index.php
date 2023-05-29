4e7c33eed7906691a2b379eb4b8d81e9931a1d56
MarcoTDS <marcodiastds@gmail.com>
2023-05-29 19:39:47 -0300

    atualização na navBar.

--- app/view/home/index.php
+++ app/view/home/index.php
@@ -7,24 +7,25 @@ require_once(__DIR__ . "/../../controller/AcessoController.php");
 require_once(__DIR__ . "/../../model/enum/UsuarioPapel.php");
 
 session_status();
-if(session_status() !== PHP_SESSION_ACTIVE) 
-{
+if (session_status() !== PHP_SESSION_ACTIVE) {
     session_start();
 }
 
 $nome = "(Sessão expirada)";
-if(isset($_SESSION[SESSAO_USUARIO_NOME]))
+if (isset($_SESSION[SESSAO_USUARIO_NOME]))
     $nome = $_SESSION[SESSAO_USUARIO_NOME];
 
 ?>
-    <?php require_once(__DIR__ . "/../include/menu.php");?>
-    <div class="container p-2 cx_meio">
-        <div class= "row" id = "apresentacao">
-            <div class ="col-12 "></div>
+<?php require_once(__DIR__ . "/../include/menu.php"); ?>
+<div class="container p-2 cx_meio">
+    <div class="row apresentacao">
+        <div class="col-6 text">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit officiis quaerat sint officia eos repellendus itaque dolor impedit quia harum dolorem ex ratione, cum quasi maiores praesentium in doloremque modi?</p>
         </div>
     </div>
 </div>
+</div>
 
-<?php  
+<?php
 require_once(__DIR__ . "/../include/footer.php");
 ?>
